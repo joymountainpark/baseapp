a8b4fe5fcfd56ef3851a70f2b4a3a182052654c3
Daniel Park <joymountainpark@gmail.com>
2015-09-21 10:48:40 +0900

    Added Email Validation on Model

--- db/migrate/20150920062559_create_users.rb
+++ db/migrate/20150920062559_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+
+  def up
     create_table :users do |t|
       t.string :name
       t.string :email
@@ -7,4 +8,9 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps null: false
     end
   end
+
+  def down
+    drop_table :users
+  end
+
 end
